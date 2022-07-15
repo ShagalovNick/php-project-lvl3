8305db3ca0370d41f737fdcb837640bb7200fae3
ShagalovNick <sekunda-61@yandex.ru>
2022-07-15 11:45:04 +0300

    edit types at table

--- database/migrations/2022_07_09_144240_create_url_checks_table.php
+++ database/migrations/2022_07_09_144240_create_url_checks_table.php
@@ -21,7 +21,7 @@ class CreateUrlChecksTable extends Migration
             $table->string('h1', 255)->nullable();
             $table->string('title', 255)->nullable();
             $table->string('description', 255)->nullable();
-            $table->timestamp('created_at');
+            $table->timestampTz('created_at', $precision = 6);
         });
 
         Schema::table('url_checks', function (Blueprint $table) {
